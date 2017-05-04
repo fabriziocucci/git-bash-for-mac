66a4d2687925b8f75a375898326faaa6a2505f8b
Fabrizio Cucci <fabrizio.cucci@gmail.com>
2017-05-04 21:15:11 +0100

    Fixed many issues
    1. Wrong link to 'git-completion.bash' and 'git-prompt.sh' scripts;
    2. Wrong filename for 'git-completion.bash' and 'git-prompt.sh' scripts;

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-curl -o ~/git-prompt.sh https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
-curl -o ~/git-completion.bash https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 # Notes about the following here document:
 # 1. in order to add a new line at the beginning of the here document, two new lines seems to be necessary;
-# 2. in order to avoid command evaluation, i.e. "$(...), the starting delimiter must be enclosed in single quotes;
+# 2. in order to avoid command evaluation, i.e. $(...), the starting delimiter must be enclosed in single quotes;
 # 3. in order to add a new line at the end of the here document, no new line is necessary.
 cat >> ~/.bash_profile <<'!'
 
