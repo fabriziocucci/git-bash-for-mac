1ce010cb748765ee8a209741cff1f5758291b014
Danni Efraim <danni@compiled.se>
2018-08-30 09:58:08 +0200

    Fixes #3 by inserting a newline to .bash_profile if it doesn't exist

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ curl -o ~/.git-bash-for-mac.sh https://raw.githubusercontent.com/fabriziocucci/g
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
+sed -i '' '$a\' ~/.bash_profile # Add a newline at the end if there isn't already one
 echo "source ~/.git-bash-for-mac.sh" >> ~/.bash_profile
