8914222cccf58055143fb031e0a0dd0bd2dd71c1
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2020-11-26 22:51:50 +0100

    Revamped zsh attachment. Removed superfluous code. Now uses zsh build in completion. Chose not to use zsh built in VCS info to keep env var support. Lots of advice found at: https://git-scm.com/book/sv/v2/Appendix-A%3A-Git-in-Other-Environments-Git-in-Zsh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ set -e
 curl -o ~/.git-bash-for-mac.sh https://raw.githubusercontent.com/fabriziocucci/git-bash-for-mac/master/.git-bash-for-mac.sh
 curl -o ~/.git-bash-for-mac-zsh.sh https://raw.githubusercontent.com/ondkloss/git-bash-for-mac/master/.git-bash-for-mac-zsh.sh
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o ~/.git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 touch ~/.bash_profile
@@ -15,5 +14,4 @@ echo "source ~/.git-bash-for-mac.sh" >> ~/.bash_profile
 touch ~/.zshrc
 # Add newline at the end of the '.zshrc' if it doesn't exist
 sed -i '' '$a\' ~/.zshrc
-echo "setopt PROMPT_SUBST" >> ~/.zshrc
 echo "source ~/.git-bash-for-mac-zsh.sh" >> ~/.zshrc
--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,6 @@
 rm ~/.git-bash-for-mac.sh
 rm ~/.git-bash-for-mac-zsh.sh
 rm ~/.git-completion.bash
-rm ~/.git-completion.zsh
 rm ~/.git-prompt.sh
 
 sed -i.backup '/source ~\/.git-bash-for-mac.sh/d' ~/.bash_profile
