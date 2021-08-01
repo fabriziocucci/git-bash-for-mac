eafc01b2b030dd5275fb9740fa39c6bc4e69e16a
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2021-08-01 14:00:57 +0200

    Added explicit PROMPT_SUBST and removed references to original repo.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl -o ~/.git-bash-for-mac.sh https://raw.githubusercontent.com/fabriziocucci/git-bash-for-mac/master/.git-bash-for-mac.sh
+curl -o ~/.git-bash-for-mac.sh https://raw.githubusercontent.com/ondkloss/git-bash-for-mac/master/.git-bash-for-mac.sh
 curl -o ~/.git-bash-for-mac-zsh.sh https://raw.githubusercontent.com/ondkloss/git-bash-for-mac/master/.git-bash-for-mac-zsh.sh
 curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
